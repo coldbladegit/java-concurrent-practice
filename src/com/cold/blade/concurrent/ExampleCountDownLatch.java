7c2850e5428c55293f116627bb1344c70d3e6f08
coldblade <cold_blade@126.com>
2017-05-23 09:46:28 +0800

    CountDownLatch

--- src/com/cold/blade/concurrent/ExampleCountDownLatch.java
+++ src/com/cold/blade/concurrent/ExampleCountDownLatch.java
@@ -9,11 +9,31 @@ import java.util.concurrent.CountDownLatch;
  */
 public class ExampleCountDownLatch {
   public static void test() {
-    
+
   }
-  
-  public long timeTasks(int nTheads, final Runnable task) {
+
+  public long timeTasks(int nThreads, final Runnable task) throws InterruptedException {
     CountDownLatch startGate = new CountDownLatch(1);
-    CountDownLatch endGate = new CountDownLatch(nTheads);
+    CountDownLatch endGate = new CountDownLatch(nThreads);
+    for (int index = 0; index < nThreads; ++index) {
+      Thread t = new Thread(new Runnable() {
+
+        @Override
+        public void run() {
+          try {
+            startGate.await();
+            task.run();
+          } catch (InterruptedException ignored) {
+          } finally {
+            endGate.countDown();
+          }
+        }
+      });
+      t.start();
+    }
+    long start = System.nanoTime();
+    startGate.countDown();
+    endGate.await();
+    return System.nanoTime() - start;
   }
 }
